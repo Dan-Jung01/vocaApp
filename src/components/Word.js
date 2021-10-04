d0f51204c4de7b54f70a04db900d250c4fb87dc9
Dan <daehan.jung01@gmail.com>
2021-10-05 03:29:10 +0900

    Update Word.js

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,55 +1,101 @@
 import { dbService } from "fbase";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import Modal from "./Modal";
 import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
 
-const Word = ({ word: w }) => {
-  const [word, setWord] = useState(w);
+const Word = ({ word }) => {
+  // const [word, setWord] = useState(w);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isDone, setIsDone] = useState(false);
-  const [isEngShow, setEngShow] = useState(true);
-  const [isKorShow, setKorShow] = useState(true);
-  const [days, setDays] = useState([]);
+  const [isDone, setIsDone] = useState(word.isDone);
+  const [isEngShow, setEngShow] = useState(word.isEngShow);
+  const [isKorShow, setKorShow] = useState(word.isKorShow);
+  const [isLoading, setIsLoading] = useState(false);
 
   const engRef = useRef(null);
   const korRef = useRef(null);
 
-  //   useEffect(() => {
-  //     dbService.collection("days").onSnapshot((snapshot) => {
-  //       const dayArray = snapshot.docs.map((doc) => ({
-  //         id: doc.id,
-  //         ...doc.data(),
-  //       }));
-  //       setDays(dayArray);
-  //     });
-  //   }, []);
+  async function KorToggleShow() {
+    await dbService.doc(`words/${word.id}`).update({
+      isKorShow: !isKorShow,
+    });
+    setKorShow(!isKorShow);
+  }
 
-  //   useEffect(() => {
-  //     dbService.collection("words").onSnapshot((snapshot) => {
-  //       const wordArray = snapshot.docs.map((doc) => ({
-  //         ...doc.data(),
-  //       }));
-  //       setWord(wordArray);
-  //     });
-  //   }, []);
+  async function EngToggleShow() {
+    await dbService.doc(`words/${word.id}`).update({
+      isEngShow: !isEngShow,
+    });
+    setEngShow(!isEngShow);
+  }
+
+  async function toggleDone() {
+    await dbService.doc(`words/${word.id}`).update({
+      isDone: !isDone,
+    });
+    setIsDone(!isDone);
+  }
+
+  const onDelecteClick = async () => {
+    const ok = window.confirm("이 단어를 삭제하시겠습니까?");
+    if (ok) {
+      await dbService.doc(`words/${word.id}`).delete();
+    }
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const saveChange = async () => {
+    if (!isLoading && engRef.current && korRef.current) {
+      const inputEng = engRef.current.value;
+      const inputKor = korRef.current.value;
+
+      console.log("asdf");
+      if (inputEng === "" && inputKor === "") {
+        alert("모든 칸을 채워주세요");
+      } else if (inputEng === "") {
+        alert("영어가 비어있습니다");
+      } else if (inputKor === "") {
+        alert("뜻이 비어있습니다");
+      } else {
+        setIsLoading(true);
+        await dbService.doc(`words/${word.id}`).update({
+          eng: inputEng,
+          kor: inputKor,
+        });
+        alert("수정이 완료 되었습니다");
+        setIsModalOpen(false);
+        setIsLoading(false);
+      }
+    }
+  };
 
   return (
     <tr className={isDone ? "off" : ""}>
-      {/* <td>
-        <input type='checkbox' checked={isDone} />
-      </td> */}
+      <td>
+        <input type='checkbox' checked={isDone} onChange={toggleDone} />
+      </td>
       <td>{isEngShow && word.eng}</td>
       <td>{isKorShow && word.kor}</td>
       <td>
-        <button>영단어 {isEngShow ? "숨기기" : "보기"}</button>
-        <button>뜻 {isKorShow ? "숨기기" : "보기"}</button>
+        <button onClick={EngToggleShow}>
+          영단어 {isEngShow ? "숨기기" : "보기"}
+        </button>
+        <button onClick={KorToggleShow}>
+          뜻 {isKorShow ? "숨기기" : "보기"}
+        </button>
       </td>
       <td>
-        <FaRegTrashAlt className='icon' />
+        <FaRegTrashAlt className='icon' onClick={onDelecteClick} />
       </td>
-      {/* <td>
-        <FaRegEdit className='icon' />
-        
+      <td>
+        <FaRegEdit className='icon' onClick={openModal} />
+
         <Modal
           open={isModalOpen}
           close={closeModal}
@@ -74,7 +120,7 @@ const Word = ({ word: w }) => {
               defaultValue={word.kor}
             />
           </div>
-          <div className='input_area'>
+          {/* <div className='input_area'>
             <label>Day</label>
             <select ref={dayRef} defaultValue={word.day}>
               {days.map((day) => (
@@ -83,9 +129,9 @@ const Word = ({ word: w }) => {
                 </option>
               ))}
             </select>
-          </div>
+          </div> */}
         </Modal>
-      </td> */}
+      </td>
     </tr>
   );
 };
