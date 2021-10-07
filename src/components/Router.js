16eed8005fa533bb24e8390ec715b043e8a2835c
Dan <daehan.jung01@gmail.com>
2021-10-08 01:01:45 +0900

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import CreateWord from "routes/CreateWord";
 import Day from "routes/Day";
@@ -9,31 +9,33 @@ import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
-    <Router>
+    <BrowserRouter>
       {isLoggedIn && <Navigation />}
-      <Switch>
-        {isLoggedIn ? (
-          <>
+      <div className='App'>
+        <Switch>
+          {isLoggedIn ? (
+            <>
+              <Route exact path='/'>
+                <Home userObj={userObj} />
+              </Route>
+              <Route exact path='/day/:day'>
+                <Day userObj={userObj} />
+              </Route>
+              <Route exact path='/profile'>
+                <Profile />
+              </Route>
+              <Route path='/create_word/:day'>
+                <CreateWord userObj={userObj} />
+              </Route>
+            </>
+          ) : (
             <Route exact path='/'>
-              <Home userObj={userObj} />
+              <Auth />
             </Route>
-            <Route exact path='/profile'>
-              <Profile />
-            </Route>
-            <Route path='/day/:day'>
-              <Day userObj={userObj} />
-            </Route>
-            <Route path='/create_word/:day'>
-              <CreateWord userObj={userObj} />
-            </Route>
-          </>
-        ) : (
-          <Route exact path='/'>
-            <Auth />
-          </Route>
-        )}
-      </Switch>
-    </Router>
+          )}
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
