d13d01754c7284054cfe79a4d1e33dc2062f59bb
Dan <daehan.jung01@gmail.com>
2021-10-08 01:01:41 +0900

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logout from "./LogOut";
 
 const Navigation = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to='/'>Home</Link>{" "}
-      </li>
-      <li>
-        <Link to='/profile'>My Profile</Link>{" "}
-      </li>
-    </ul>
+  <nav className='navContainer'>
+    <div className='navLeftSide'>
+      <h1 className='navTitle'>
+        <Link to='/'>나만의 영어단어장</Link>
+      </h1>
+      <ul className='navMenu'>
+        <li>
+          <Link to='/'>Home</Link>{" "}
+        </li>
+        <li>
+          <Link to='/profile'>My Profile</Link>{" "}
+        </li>
+      </ul>
+    </div>
+    <div className='navRightSide'>
+      <Logout />
+    </div>
   </nav>
 );
 export default Navigation;
