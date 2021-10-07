8408e1491b29339bb9ea5c66e0dfc3b0fd200d02
Dan <daehan.jung01@gmail.com>
2021-10-08 01:02:01 +0900

    Update CreateWord.js

--- src/routes/CreateWord.js
+++ src/routes/CreateWord.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { dbService } from "fbase";
 
-const CreateWord = () => {
+const CreateWord = ({ userObj }) => {
   const history = useHistory();
   const [isLoading, setIsLoading] = useState(false);
   const { day } = useParams();
@@ -26,8 +26,11 @@ const CreateWord = () => {
       } else {
         setIsLoading(true);
         await dbService
+          .collection("users")
+          .doc(userObj.uid)
           .collection("words")
           .add({
+            creatorId: userObj.uid,
             day: day,
             eng,
             kor,
@@ -45,23 +48,27 @@ const CreateWord = () => {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className='input_area'>
-        <label>Eng</label>
-        <input type='text' placeholder='영어를 입력해주세요' ref={engRef} />
-      </div>
-      <div className='input_area'>
-        <label>Kor</label>
-        <input type='text' placeholder='뜻을 입력해주세요' ref={korRef} />
-      </div>
-      <button
-        style={{
-          opacity: isLoading ? 0.3 : 1,
-        }}
-      >
-        {isLoading ? "Saving..." : "저장"}
-      </button>
-    </form>
+    <div className='cwContainer'>
+      <form onSubmit={onSubmit} className='cwform'>
+        <label className='cwTitle'> 새로운 단어를 추가해주세요 </label>
+        <div className='input_area'>
+          <label>Eng</label>
+          <input type='text' placeholder='영어를 입력해주세요' ref={engRef} />
+        </div>
+        <div className='input_area'>
+          <label>Kor</label>
+          <input type='text' placeholder='뜻을 입력해주세요' ref={korRef} />
+        </div>
+        <button
+          className='cwSave'
+          style={{
+            opacity: isLoading ? 0.3 : 1,
+          }}
+        >
+          {isLoading ? "Saving..." : "저장"}
+        </button>
+      </form>
+    </div>
   );
 };
 export default CreateWord;
