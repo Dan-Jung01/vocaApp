33d635bd5e82bac6b845ae246961c6b74297f6ad
Dan <daehan.jung01@gmail.com>
2021-10-08 01:02:10 +0900

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,44 +1,68 @@
 import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { IoAddCircleSharp } from "react-icons/io5";
+import Day from "es-abstract/5/Day";
 
 const Home = ({ userObj }) => {
   const [days, setDays] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    dbService.collection("days").onSnapshot((snapshot) => {
-      const dayArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setDays(dayArray);
-    });
-  }, []);
+    dbService
+      .collection("users")
+      .doc(userObj.uid)
+      .collection("dayss")
+      .orderBy("day")
+      .onSnapshot((snapshot) => {
+        const dayArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setDays(dayArray);
+      });
+  }, [userObj]);
 
   // Add another day if user click button
   const onClick = async (e) => {
-    await dbService.collection("days").add({
-      day: days.length + 1,
-      creatorId: userObj.uid,
-    });
+    setIsLoading(true);
+    await dbService
+      .collection("users")
+      .doc(userObj.uid)
+      .collection("dayss")
+      .add({
+        day: days.length + 1,
+      })
+      .then(() => {
+        setIsLoading(false);
+      });
   };
 
-  console.log(userObj.displayName);
-
   return (
-    <ul className='list_day'>
-      Home
-      {days.map((day) =>
-        day.creatorId === userObj.uid ? (
-          <li className='each_day' key={day.id}>
-            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-          </li>
-        ) : null
-      )}
-      <button onClick={onClick} name='addDay'>
-        Add Day
-      </button>
-    </ul>
+    <div className='homeContainer'>
+      <div className='homeTitle'> 안녕하세요 {userObj.displayName} 님 </div>
+      <ul className='homeDayContainer'>
+        {days.map(
+          (day) => (
+            // day.creatorId === userObj.uid ? (
+            <li className='homeEachDay' key={day.id}>
+              {/* <Day /> */}
+              <Link
+                to={{
+                  pathname: `/day/${day.day}`,
+                }}
+              >
+                Day {day.day}
+              </Link>
+            </li>
+          )
+          // ) : null
+        )}
+        <button className='homeAddDay' onClick={onClick}>
+          Add Day <IoAddCircleSharp size={30} />
+        </button>
+      </ul>
+    </div>
   );
 };
 export default Home;
