bacc67f3b077361cb8797c2c3698a35921f139db
Dan <daehan.jung01@gmail.com>
2021-10-12 23:20:27 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import "./style.css";
-// import fbase from "./fbase";
-// console.log(fbase);
 
 ReactDOM.render(
   <BrowserRouter>
